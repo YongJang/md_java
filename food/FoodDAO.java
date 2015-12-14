b9480e6d7817b491ce6843957ca6ad962dc2dc53
YongJang <yj_430@naver.com>
2015-12-15 06:21:48 +0900

    Food DAO 수정

--- food/FoodDAO.java
+++ food/FoodDAO.java
@@ -64,6 +64,179 @@ public class FoodDAO{
 			close();
 		}
 	}
+	
+	public Vector<FoodDTO> getFoodListbyRate() throws SQLException{
+		String sql = "select * from food order by rate desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyRegdateDesc() throws SQLException{
+		String sql = "select * from food order by regdate desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyRegdateAsc() throws SQLException{
+		String sql = "select * from food order by regdate asc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyCostDesc() throws SQLException{
+		String sql = "select * from food order by cost desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyCostAsc() throws SQLException{
+		String sql = "select * from food order by cost asc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyNameAsc() throws SQLException{
+		String sql = "select * from food order by name asc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyNameDesc() throws SQLException{
+		String sql = "select * from food order by name desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyTypeDesc() throws SQLException{
+		String sql = "select * from food order by type desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyTypeAsc() throws SQLException{
+		String sql = "select * from food order by type asc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyRegionAsc() throws SQLException{
+		String sql = "select * from food order by region asc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListbyRegionDesc() throws SQLException{
+		String sql = "select * from food order by region desc";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListwithCostunder(int cost) throws SQLException{
+		String tempNum = Integer.toString(cost);
+		String sql = "select * from food where cost<?";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, tempNum);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+	
+	public Vector<FoodDTO> getFoodListwithName(String name) throws SQLException{
+		String tempName = name;
+		String sql = "select * from food where name like '%"+tempName+"%'";
+		try{
+			connect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			Vector<FoodDTO> list = makeList(rs);
+			return list;
+		}finally{
+			close();
+		}
+	}
+
 
 	public Vector<FoodDTO> makeList(ResultSet rs) throws SQLException {
 	      	// ResultSet -> Vector<FoodDTO> º¯È¯
